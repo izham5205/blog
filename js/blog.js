c4b3c4f7b5143bf3a85e52c630eda3434b9b0ae6
Mohd Izham <izham5205@gmail.com>
2015-04-28 19:52:05 +0800

    add blog

--- js/blog.js
+++ js/blog.js
@@ -6,9 +6,22 @@ $(function() {
     Parse.initialize("fiS8Iexr1h5PT9znMHRLbjOzRqHovmMfaGKTDHCi", "GZlWlwLav9WaaLBm41PIAQqFTpTUGwAVs1drUwOV");
  
     var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
+	var testObject = new TestObject();
+	testObject.save({foo: "bar"}).then(function(object) {
       alert("yay! it worked");
     });
+	var Blog = Parse.Object.extend("Blog");
+	var Blogs = Parse.Collection.extend({
+    model: Blog
+	});
+	var blogs = new Blogs();
+	blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
+});
  
 });
